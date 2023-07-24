fff34327a90165c2d1ed9583a804ec908834bc91
LenineKawai <maxgolinvaux@gmail.com>
2023-07-24 05:39:02 -0400

    refactor: adding tailwind css

--- srcs/client/src/Game.tsx
+++ srcs/client/src/Game.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import Konva from 'konva';
+import { Stage, Layer, Text, Star } from 'react-konva';
 
 export function Game() {
+
+  
   return (
-    <section className="section">
-    <div className="columns is-mobile is-centered">
-      <div className="column is-half is-centered has-background-primary">
-          <canvas id="myCanvas" width="100%" height="600"></canvas>
-      </div>
-    </div>
-  </section>
+    <Stage className='test' width={window.innerWidth / 2} height={window.innerHeight / 2}>
+      <Layer>
+      <Star
+            key={1}
+            id={'feur'}
+            x={100}
+            y={100}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+          />
+      </Layer>
+    </Stage>
   );
 };
 
