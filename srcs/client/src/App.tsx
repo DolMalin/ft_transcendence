29fee9eea2c13862b3c4e7fbcddd53566081a767
DolMalin <dolmalinn@gmail.com>
2023-11-15 13:57:19 +0100

    feat: chakra ui

--- srcs/client/src/App.tsx
+++ srcs/client/src/App.tsx
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 
 import './App.css';
 import {CookiesProvider, useCookies}  from 'react-cookie';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./auth/Auth"
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 
   const [cookies, setCookie] = useCookies(["access_token"])
 
   return (<>
-    {<Auth />}
+    <ChakraProvider>
+      <Auth />
+    </ChakraProvider>
   </>
   );
 }
