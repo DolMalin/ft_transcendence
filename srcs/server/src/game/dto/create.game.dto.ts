f6225570c6673f9013f8497c8fbcf91a832e7f7f
DolMalin <dolmalinn@gmail.com>
2023-12-12 17:11:45 +0100

    feat: satanize and protect game dtos

--- srcs/server/src/game/dto/create.game.dto.ts
+++ srcs/server/src/game/dto/create.game.dto.ts
@@ -1,22 +1,35 @@
-import { IsNumber, IsString } from "class-validator";
-
+import { IsNumber, IsString, IsUUID, MaxLength, MinLength, Matches, IsPositive} from "class-validator";
+import { Transform, Type } from 'class-transformer'
+import * as sanitizeHtml from 'sanitize-html'
+import { TransformFnParams } from 'class-transformer'
 export class CreateGameDto {
 	
-    @IsString()
+    @IsUUID()
     winnerId : string;
-
+    
     @IsString()
-    winnerUsername : string;
-
-    @IsString()
+    @IsUUID()
     looserId : string;
 
     @IsString()
+	@Transform((params: TransformFnParams) => sanitizeHtml(params.value))
+	@MinLength(6)
+	@MaxLength(20)
+	@Matches(/^[^#<>\[\]|{}\/@:=]*$/, {message: 'username must not contains ^ # < > [ ] | { } / @ or :'})
+    winnerUsername : string;
+
+    @IsString()
+	@Transform((params: TransformFnParams) => sanitizeHtml(params.value))
+	@MinLength(6)
+	@MaxLength(20)
+	@Matches(/^[^#<>\[\]|{}\/@:=]*$/, {message: 'username must not contains ^ # < > [ ] | { } / @ or :'})
     looserUsername : string;
 
     @IsNumber()
+    @IsPositive()
     winnerScore : number;
 
     @IsNumber()
+    @IsPositive()
     looserScore : number;
 }
