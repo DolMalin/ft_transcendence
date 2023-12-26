db9bc95ac18f2bf1dcef5a0d5505d8b06f07e4ac
DolMalin <dolmalinn@gmail.com>
2023-12-26 17:28:07 +0100

    fix: rename logout-2fa to 2fa/logout

--- srcs/server/src/auth/controllers/auth.controller.ts
+++ srcs/server/src/auth/controllers/auth.controller.ts
@@ -37,12 +37,6 @@ export class AuthController {
       return this.authService.refresh(user, res)
     }
 
-    @UseGuards(AccessToken2FAGuard)
-    @Get('logout-2fa')
-    logout2fa(@GetUser() user: User, @Res() res: any) {
-      return this.authService.logout(user, res)
-    }
-
     @UseGuards(AccessTokenGuard)
     @Get('logout')
     logout(@GetUser() user: User, @Res() res: any) {
@@ -80,6 +74,12 @@ export class AuthController {
     @Post('2fa/login')
     async twoFactorAuthenticationLogin(@GetUser() user: User, @Res() res:any, @Body() body:any) {
       return await this.authService.twoFactorAuthenticationLogin(user, res, body)
+    }
+
+    @UseGuards(AccessToken2FAGuard)
+    @Get('2fa/logout')
+    logout2fa(@GetUser() user: User, @Res() res: any) {
+      return this.authService.logout(user, res)
     }
 
     @UseGuards(AccessTokenGuard)
